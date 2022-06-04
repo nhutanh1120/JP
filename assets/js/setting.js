a619021b2c9f421e759e10ba0977520dbf20e2c6
nhutanh1120 <nhutanh1120@gmail.com>
2022-06-04 18:50:41 +0700

    update page loading effect

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -1,14 +1,21 @@
-(() => {
-    const currentStatus = localStorage.getItem('darkmode');
-    if (currentStatus) {
-        document.documentElement.setAttribute('data-theme', currentStatus);
-    }
-})();
-
 document.oncontextmenu = (event) => {
     event.preventDefault();
     return false;
 }
+$(function () {
+    (() => {
+        $('#loader').css({ 'display': 'none' });
+        const currentStatus = localStorage.getItem('darkmode');
+        if (currentStatus) {
+            document.documentElement.setAttribute('data-theme', currentStatus);
+            setTimeout(function () {
+                if (currentStatus === 'dark' && $('lord-icon').attr('src')) {
+                    $('lord-icon').attr({ 'colors': 'primary:#fff' });
+                }
+            }, 2000);
+        }
+    })();
+});
 
 $('.setting-icon').click(function () {
     $(this).find('i').toggleClass('bx-spin');
@@ -20,8 +27,10 @@ $('#slidebar').click(function () {
     if (currentStatus === 'light') {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('darkmode', 'dark');
+        $('lord-icon').attr({ 'colors': 'primary:#fff' });
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('darkmode', 'light');
+        $('lord-icon').attr({ 'colors': '' });
     }
 });
